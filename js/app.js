374f2a9e6dc1899859bdc82a75d0e32942705aa9
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-07-31 15:11:17 -0300

    map iframe

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-// La funcionalidad de tu proyecto
+// function initialize() {
+//     var mapOptions = {
+//       center: new google.maps.LatLng(-34.397, 150.644),
+//       zoom: 8,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     var map = new google.maps.Map(document.getElementById("map"),
+//         mapOptions);
+// }
+//
+//
+// function loadScript() {
+//   var script = document.createElement("script");
+//   script.type = "text/javascript";
+//   script.src ="http://maps.googleapis.com/maps/api/js?key=AIzaSyBx9uDklM5_vSZHWkm8sqpx0Su4h1Xw3SE";
+//
+//   document.body.appendChild(script);
+// }
+//
+//   window.onload = loadScript;
